1eb1f2e970cab75b934a611c95674506dbaaf85e
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2026-03-10 18:18:27 +0200

    Get title on answers

--- frontend/src/components/Answers.jsx
+++ frontend/src/components/Answers.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector} from 'react-redux'
 //import { Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 
+import { initializeBasic } from '../reducers/basicReducer'
+import { initializeComprehensive} from '../reducers/comprehensiveReducer'
 import { initializeAnswers } from '../reducers/answersReducer'
 
 import Notification from './Notification';
@@ -13,10 +15,12 @@ const Answers = () => {
   const answers = useSelector(state => state.answers)
   const user = useSelector(state => state.user)
   const basic = useSelector(state => state.basic)
-  const comnprehensive = useSelector(state => state.comprehensive)
-  const [basicOrInclusive, setBasicOrInclusive] = useState()
+  const comprehensive = useSelector(state => state.comprehensive)
+  const [basicOrInclusive, setBasicOrInclusive] = useState(null)
 
   useEffect(() => {
+    dispatch(initializeBasic())
+    dispatch(initializeComprehensive())
     dispatch(initializeAnswers())
   }, [dispatch])
 
@@ -28,59 +32,107 @@ const Answers = () => {
   if (!answers)
     return <div>Loading...</div>
 
-  return (
-    <div>
+  const module = (basicOrInclusive !== null
+    && basicOrInclusive === 'basic_module')
+    ? basic
+    : comprehensive
+ // console.log(`module: ${JSON.stringify(module)}`)
+
+
+  // Sort answers based on sectionId and questionId
+  const sortedAnswers = [...answers].sort((a, b) => {
+    const questionA = module
+      .flatMap(m => m.sections)
+      .flatMap(s => s.questions)
+      .find(q => q.id === a.questionId)
+
+    const questionB = module
+      .flatMap(m => m.sections)
+      .flatMap(s => s.questions)
+      .find(q => q.id === b.questionId)
+
+    if (!questionA || !questionB)
+      return 0
+
+    // Search for indexes
+    const sectionA = module.flatMap(m => m.sections).find(s =>
+      s.questions.some(q => q.id === a.questionId))
+    const sectionB = module.flatMap(m => m.sections).find(s =>
+      s.questions.some(q => q.id === b.questionId))
+
+    const sectionIndexA = module.flatMap(m => m.sections).indexOf(sectionA)
+    const sectionIndexB = module.flatMap(m => m.sections).indexOf(sectionB)
+
+    if (sectionIndexA !== sectionIndexB) {
+      return sectionIndexA - sectionIndexB
+    }
+
+    // If sections are same, sort by question index
+    const questionIndexA = sectionA.questions.indexOf(questionA)
+    const questionIndexB = sectionB.questions.indexOf(questionB)
+
+    return questionIndexA - questionIndexB
+  })
+
+  console.log(`basicOrInclusive: ${basicOrInclusive}`)
+
+
+  return ( <div>
       <h2>{user.name} vastaukset:</h2>
 
-      {answers.map((a, idx) => {
-        if (a.moduleId === 'basic_module') {
-          const question = basic
-            .flatMap(b => b.sections)
-            .flatMap(s => s.questions)
-            .find(q => q.id === a.questionId)
+      {sortedAnswers.map((a, idx) => {
+        const question = module
+          .flatMap(m => m.sections)
+          .find(q => q.id === a.s)
 
-          return (
-            <div key={`basic-${idx}`}>
-              <p>Kysymys: <strong>{question?.question || a.questionId}</strong></p>
-              {a.type !== 'group' ? (
-                <p>Vastaus: {a.type === 'boolean' ? (a.answer ? 'Kyllä' : 'Ei' ) : a.answer}</p>
-              ) : (
-                <>
-                  {question?.sub_questions.map((subQs, subQsIdx) => {
-                    const groupAnswer = a.groupAnswers
-                      .find(ga => ga.subQuestionId === subQs.id)
+        return (
+          <div key={`basic-${idx}`}>
+            <p>Osio: {question?.title}</p>
+            <p>Kysymys: <strong>{question?.question || a.questionId}</strong></p>
+            {a.type !== 'group' ? (
+              <p>Vastaus: {a.type === 'boolean' ? (a.answer ? 'Kyllä' : 'Ei' ) : a.answer}</p>
+            ) : (
+              <>
+                <p>Alakysymykset:</p>{question?.sub_questions.map((subQs, subQsIdx) => {
+                  const groupAnswer = a.groupAnswers
+                    .find(ga => ga.subQuestionId === subQs.id)
 
-                    if (!groupAnswer) return null;
+                  if (!groupAnswer)
+                    return null;
 
-                    return (
-                      <div key={`subQs-${subQsIdx}`}>
-                        <p>Alakysymys: <strong>{subQs.category}</strong></p>
-                        {Object.entries(groupAnswer.values).map(([fieldId, fieldData], fIdx) => {
-                          const field = subQs.fields.find(f => f.id === fieldId)
-                          if (!field)
-                            return null
+                  return (
+                    <div key={`subQs-${subQsIdx}`}>
+                      <p>Alakysymys: <strong>{subQs.category}</strong></p>
+                      {Object.entries(groupAnswer.values).map(([fieldId, fieldData], fIdx) => {
+                        const field = subQs.fields.find(f => f.id === fieldId)
+                        if (!field)
+                          return null
 
-                          return (
-                            <div key={`subQsField-${fIdx}`}>
-                              <p>{field.label}: {fieldData.value}</p>
-                            </div>
-                          )
-                        })}
-                      </div>
-                    )
-                  })}
-                </>
-              )}
-            </div>
-            )
-          }
-      })}
-    </div>
+                        return (
+                          <div key={`subQsField-${fIdx}`}>
+                            <p>{field.label}: {fieldData.value}</p>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
+              </>
+            )}
+          </div>
+          )
+        }
+    )}
+  </div>
   )
 
 }
 
 /*
+        const question = module
+          .flatMap(m => m.sections)
+          .flatMap(s => s.questions)
+          .find(q => q.id === a.questionId)
  * p
                   <p>Alakysymykset:</p>
                   {a.groupAnswers.map((ga, gaIdx) => (
